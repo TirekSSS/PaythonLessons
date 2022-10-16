fbb0940d54c5a811f003472122f38483b166b741
666 Imposter <115923565+TirekSSS@users.noreply.github.com>
2022-10-16 10:47:14 +0000

    add human class

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('hello world')
+#print('hello world')
+class Human:
+  def __init__(self):
+    self.name = 'None'
+    self.age = 0
+    self.gender = 'None'
+  def introduce(self):
+    print('Hello? ma name is', self.name)
+  def add_info(self):
+    self.name = input('Name: ')
+    self.age = int(input('Age: '))
+    self.gender = input('Gender')
+
+obj = Human()
+obj.introduce()
+obj.add_info()
+obj.introduce()
