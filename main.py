acccef347b7c93ac2b74120b51a71d328b9cbf9c
666 Imposter <115923565+TirekSSS@users.noreply.github.com>
2022-10-23 10:04:59 +0000

    add class:human, child and parent

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+class Human:
+	def __init__(self, name):
+		self.name = name
+		self.gender = 'None'
+		self.age = 0
+	def live(self):
+		print(self.name, 'is alive')
+
+class Parent(Human):
+	def work():
+		print('I can work')
+
+class Child(Human):
+	def happybirthday(self):
+		self.age +=1 
+		print('I am', self.age)
+	def study(self):
+		print(self.name,' can study')
+
+obj = Child('Bob')
+obj.study()
+obj.live()
+obj.happybirthday()
+obj.happybirthday()
+obj.happybirthday()
+
 # a = 4
 # print('a')
 # print(a)
@@ -34,78 +60,78 @@
 # obj.add_info()
 # obj.introduce()
 
-from random import *
+# from random import *
 
-class University:
-	def __init__(self, title, faculty):
-		self.title = title
-		self.faculty = faculty
-		self.budget = False;
-	def check_progress(self, student):
-		if student > 3:
-			self.budget = True
-		self.isbudget()
+# class University:
+# 	def __init__(self, title, faculty):
+# 		self.title = title
+# 		self.faculty = faculty
+# 		self.budget = False;
+# 	def check_progress(self, student):
+# 		if student > 3:
+# 			self.budget = True
+# 		self.isbudget()
 
-	def isbudget(self):
-		if self.budget == True:
-			print('Congratulation! You are on budget!')
+# 	def isbudget(self):
+# 		if self.budget == True:
+# 			print('Congratulation! You are on budget!')
 
-class Student:
-	def __init__(self, name):
-		self.name = name
-		self.gladness = 50
-		self.progress = 0
-		self.alive = True
+# class Student:
+# 	def __init__(self, name):
+# 		self.name = name
+# 		self.gladness = 50
+# 		self.progress = 0
+# 		self.alive = True
 
-	def ask_budget(self, university):
-		university.check_progress(self.progress)
+# 	def ask_budget(self, university):
+# 		university.check_progress(self.progress)
 		
-	def study(self):
-		print('Study time')
-		self.progress += 0.12
-		self.gladness -= 5
+# 	def study(self):
+# 		print('Study time')
+# 		self.progress += 0.12
+# 		self.gladness -= 5
 		
-	def sleep(self):
-		print('Sleep time')
-		self.gladness += 3
+# 	def sleep(self):
+# 		print('Sleep time')
+# 		self.gladness += 3
 		
-	def chill(self):
-		print('Chill time')
-		self.gladness += 5
-		self.progress -= 0.1
+# 	def chill(self):
+# 		print('Chill time')
+# 		self.gladness += 5
+# 		self.progress -= 0.1
 		
-	def is_alive(self):
-		if self.progress < -0.5:
-			print('Sooo bad')
-			self.alive = False
-		elif self.gladness <= 0:
-			print('Depression...')
-			self.alive = False
-		elif self.progress > 5:
-			print('Passed the exam!')
-			self.alive = False
+# 	def is_alive(self):
+# 		if self.progress < -0.5:
+# 			print('Sooo bad')
+# 			self.alive = False
+# 		elif self.gladness <= 0:
+# 			print('Depression...')
+# 			self.alive = False
+# 		elif self.progress > 5:
+# 			print('Passed the exam!')
+# 			self.alive = False
 		
-	def end(self):
-		print('Gladness:', self.gladness)
-		print('Progress:', self.progress)
+# 	def end(self):
+# 		print('Gladness:', self.gladness)
+# 		print('Progress:', self.progress)
 		
-	def live(self, day):
-		print('Day:',day)
-		if day % 10 == 0:
-			self.ask_budget(univer)
-		live_cube = randint(1,3)
-		if live_cube == 1:
-			self.study()
-		elif live_cube == 2:
-			self.sleep()
-		elif live_cube == 3:
-			self.chill()
-		self.end()
-		self.is_alive()
+# 	def live(self, day):
+# 		print('Day:',day)
+# 		if day % 10 == 0:
+# 			self.ask_budget(univer)
+# 		live_cube = randint(1,3)
+# 		if live_cube == 1:
+# 			self.study()
+# 		elif live_cube == 2:
+# 			self.sleep()
+# 		elif live_cube == 3:
+# 			self.chill()
+# 		self.end()
+# 		self.is_alive()
 
-obj = Student('Bob')
-univer = University('Step Univer', 'Computer Science')
-for day in range(365):
-	if obj.alive == False:
-		break
-	obj.live(day)
+# obj = Student('Bob')
+# univer = University('Step Univer', 'Computer Science')
+# for day in range(365):
+# 	if obj.alive == False:
+# 		break
+# 	obj.live(day)
